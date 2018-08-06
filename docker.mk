66dc2d188d9c445852c57e61252e5ab2dd61df51
root <39826573+ltkum@users.noreply.github.com>
2018-08-06 12:28:14 +0000

    refactor: all functions are now called with the following order for parameters [mako_function], [image_name], [image_tag], [other variables]

--- docker.mk
+++ docker.mk
@@ -9,20 +9,20 @@ Steo 3; Profit\
 define dockerhelp
 
 	@echo "The goal of this library is to provide functions to harmonize and ease the use of docker in the different swisstopo projects. This help should tell you how to use the different functions."
-	@echo "dockerbuild : called with call dockerbuild, [dev|int|prod], [mako_cmd], [image_base_name], [additional variables]"
+	@echo "dockerbuild : called with call dockerbuild,[mako_cmd],[image_base_name], [dev|int|prod], [additional variables]"
 	@echo "[dev|int|prod] is the staging environment. it defines both the tag of the built image and the 'staging' variable for mako to replace in your docker-compose.yml.in."
 	@echo "[mako_cmd] refers to the mako command. it is, usually, in your python virtual environment in $${python_directory}/bin/mako-render"
 	@echo "[image_base_name] will give a image_base_name variable for the mako render which will be swisstopo/[image_base_name]."
 	@echo "[additional variables] should be a string in the following form: --var 'var_name=value' --var 'var_name_2=value_2' etc. The goal is to provide all variables that are specific to your project. "
 	@echo "Example of use : call dockerbuild, dev, .venv/bin/mako-render, service-example,--var 'ci=true') could build the swisstopo/service-example:dev and swisstopo/service-example-nginx:dev images"
-	@echo "dockerrun: called with call dockerrun, [dev | int | prod], [mako_cmd], [image_base_name], [additional variables]"
+	@echo "dockerrun: called with call dockerrun, [mako_cmd], [image_base_name],[dev | int | prod],  [additional variables]"
 	@echo "Same function as dockerbuild, but it will run the images in containers."
 	@echo "dockerpurge : called with call dockerpurge, [image_name]"
 	@echo "it will remove all containers running and images whose image name"
 	@echo "are swisstopo/[image_name] with any tag."
-	@echo "dockerpush: called with call dockerpush, [image_name],[tag]"
+	@echo "dockerpush: called with call dockerpush, [image_name], [tag]"
 	@echo "will push to the swisstopo dockerhub the swisstopo/[image_name]:[tag] image."
-	@echo "dockerdeploy: called with call call dockerdeploy, [dev | int | prod], [mako_cmd], [image_base_name]"
+	@echo "dockerdeploy: called with call call dockerdeploy,[mako_cmd], [image_base_name], [dev | int | prod], [additional variables]"
 endef
 
 define dockerbuild
@@ -55,26 +55,24 @@ define dockerpush
 	done
 endef
 
-#Okay, this is ugly, but it should work
+#Okay, this is ugly, but it works
 
 define dockerpipe
-	$(call dockerpurge,${3}) 
+	$(call dockerpurge,${2}) 
 	$(call dockerbuild,${1},${2},${3},${4})
-	for line in $$(sudo docker images --format "{{.Repository}}:{{.Tag}}" | grep "swisstopo" | grep ${3} | grep ${1}) ; do \
+	for line in $$(sudo docker images --format "{{.Repository}}:{{.Tag}}" | grep "swisstopo" | grep ${2} | grep ${3}) ; do \
 	sudo docker tag $$line "$$line"_$$(date +%Y_%m_%d) ; \
 	$(call dockerpush,"$$line_"$$(date +%Y_%m_%d),) ; \
 	done ;
 endef
 
-# $1 is the environment (dev, int, prod) and $2 is the base name of the image built \
-# $3 is the MAKO COMMAND\
-# How to use : the env file in (usually) ${1}.env and should be specified in the docker-compose \
-# in the form ${staging}.env. If multiples environment files are used, make sure they have the \  
-# same nomenclature to call all your needed env files. All the necessary variables to use in your \
-# docker-compose should be in your .env file. That way, we have a simple clean unique function. \
-# please ? 
+# $1 is the MAKO COMMAND and $4 is the base name of the image built \
+# $3 is the environment (dev, int, prod) and $2 is the variable that tells the docker compose if he \
+# is preparing to deploy to rancher.
+# $5 is composed of all variables used in the docker compose in the format 
+# --var "var_1=val_1" --var "var_2=val_2" --var "var_3=val_3"
+
 define docker-compose.yml
-	sudo echo "${3} | ${2} | ${1} | ${4} | ${5}"
-        sudo ${3} docker-compose.yml.in --var "rancher_deploy=${2}" --var "environment=${1}" --var "image_base_name=${4}" ${5} > docker-compose.yml
+        sudo ${1} docker-compose.yml.in --var "rancher_deploy=${2}" --var "image_base_name=${3}" --var "environment=${4}" ${5} > docker-compose.yml
 endef
 
