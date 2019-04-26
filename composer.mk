e766f829112544ca99bdd91e76e513531896a814
root <39826573+ltkum@users.noreply.github.com>
2019-04-26 14:10:13 +0000

    support for env files. not yet working

--- composer.mk
+++ composer.mk
@@ -23,3 +23,24 @@ define docker-compose.yml
 	sudo ${MAKO_CMD} ${2} ${1}/docker-compose.yml.in  > ${1}/docker-compose.yml
 endef
 
+# create env file logic : \
+1 - Is there a ${staging}.env.in file ? yes -> go to 2 \
+2 - Is there a credentials.${staging}.arg file ? yes -> got to 3, no -> go to 4 \
+3 - MAKO credentials.staging.arg staging.env.in > staging.env \
+4 - Is there a credentials.arg file ? yes -> go to 5, no -> go to 6 \
+5 - MAKO credentials.arg staging.env.in > staging.env \
+6 - cp staging.env.in staging.env
+define create_env_file
+        env_template="${2}.env.in"
+        cred_staged_file="credentials.${2}.arg"
+        cred_file="credentials.arg"
+	if test "$$(ls -la ${1}/ | grep "${env_template}")" != ""; then \
+		if test "$$(ls -la ${1} | grep "${cred_staged_file}")" != ""; then \
+			sudo ${MAKO_CMD} ${cred_staged_file} ${1}/${env_template} > ${1}/${staging}.env \                
+		elif test "$$(ls -la ${1} | grep "${cred_file}")" != ""; then \
+			sudo ${MAKO_CMD} ${cred_file} ${1}/${env_template} > ${1}/${staging}.env \
+		else \
+			cp "${1}/${env_template}" "${1}/${2}.env" \
+		fi \
+	fi
+endef
